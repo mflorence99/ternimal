09c67ce6e9a44c5a5ab8942fda554476653bc6be
Mark Florence <mflo999@gmail.com>
2020-07-22 04:58:53 -0400

    More refactoring

--- renderer/app/barrel.ts
+++ renderer/app/barrel.ts
@@ -1,20 +1,24 @@
 import { IconsModule } from './icons';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { ContextMenuModule } from 'ngx-contextmenu';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 const MODULES = [
+  BrowserAnimationsModule,
   BrowserModule,
   ContextMenuModule,
   FontAwesomeModule,
   FormsModule,
   IconsModule,
   MatButtonModule,
+  MatTooltipModule,
   ReactiveFormsModule
 ];
 
