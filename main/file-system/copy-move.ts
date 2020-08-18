71bfadf67bd406cb7218d0673b3c58e94a1a4173
Mark Florence <mflo999@gmail.com>
2020-08-18 07:30:05 -0400

    Cleaned up copy + move

--- main/file-system/copy-move.ts
+++ main/file-system/copy-move.ts
@@ -29,63 +29,81 @@ const copyAndMover = async (
 
   const moveOpts = { overwrite: false };
 
-  // what to write to?
-  let stat = await fs.lstat(to);
-  to = stat.isDirectory() ? to : path.dirname(to);
-
-  // make sure it is writable
-  try {
-    await fs.access(to, fs.constants.W_OK);
-  } catch (err) {
-    theWindow?.webContents.send(Channels.error, err.message);
-    return;
-  }
-
-  // itemize each of the "froms" and match them to a "to"
-  const ifroms = [],
-    itos = [];
-  const cleanups = [],
-    tos = [];
-  for (const from of froms) {
-    stat = await fs.lstat(from);
-    if (stat.isDirectory()) {
-      if (op === 'move') cleanups.push(from);
-      const itemized = (await util.promisify(recursive)(from)) as string[];
-      const root = path.dirname(from);
-      itemized.forEach((ifrom) => {
-        ifroms.push(ifrom);
-        itos.push(path.join(to, ifrom.substring(root.length)));
-      });
-      tos.push(path.join(to, from.substring(root.length)));
-    } else {
-      ifroms.push(from);
-      const ito = path.join(to, path.basename(from));
-      itos.push(ito);
-      tos.push(ito);
-    }
-  }
-
-  // kick off the long-running op
   const longRunningOp: LongRunningOp = {
     id,
     item: null,
     progress: 0,
     running: true
   };
-  theWindow?.webContents.send(Channels.longRunningOpProgress, longRunningOp);
 
-  // copy or move froms => to
-  async.eachOfSeries(
-    ifroms,
-    async (ifrom: string, ix: number) => {
+  const cleanups = [],
+    tos = [];
+
+  const ifroms = [],
+    itos = [];
+
+  try {
+    // what to write to?
+    let stat = await fs.lstat(to);
+    to = stat.isDirectory() ? to : path.dirname(to);
+
+    // make sure it is writable
+    await fs.access(to, fs.constants.W_OK);
+
+    // match all the "froms" to a "to"
+    await async.eachSeries(froms, async (from) => {
+      stat = await fs.lstat(from);
+      if (stat.isDirectory()) {
+        // NOTE: convenient here to mark which directories
+        // must be cleaned up after a move
+        if (op === 'move') cleanups.push(from);
+        const root = path.dirname(from);
+        tos.push(path.join(to, from.substring(root.length)));
+      } else tos.push(path.join(to, path.basename(from)));
+    });
+
+    // if the "to" already exists, we have to disambiguate it
+    await async.eachOfSeries(tos, async (to, ix) => {
+      const parsed = path.parse(to);
+      // see if we are already disambiguated with this pattern
+      let disamb = 1;
+      // eslint-disable-next-line @typescript-eslint/prefer-regexp-exec
+      const parts = parsed.name.match(/^(.+) \(([\d]+)\)$/);
+      if (parts) {
+        disamb = Number(parts[2]);
+        parsed.name = parts[1];
+      }
+      while (await fs.pathExists(tos[ix]))
+        tos[ix] = path.join(
+          parsed.dir,
+          `${parsed.name} (${disamb++})${parsed.ext}`
+        );
+    });
+
+    // itemize each of the "froms" that are a direcory
+    await async.eachOfSeries(froms, async (from, ix) => {
+      stat = await fs.lstat(from);
+      if (stat.isDirectory()) {
+        const itemized = (await util.promisify(recursive)(from)) as string[];
+        const root = path.dirname(from);
+        itemized.forEach((ifrom) => {
+          ifroms.push(ifrom);
+          itos.push(path.join(tos[ix], ifrom.substring(root.length)));
+        });
+      } else {
+        ifroms.push(from);
+        itos.push(tos[ix]);
+      }
+    });
+
+    // kick off the long-running op
+    theWindow?.webContents.send(Channels.longRunningOpProgress, longRunningOp);
+
+    // copy or move froms => to
+    await async.eachOfSeries(ifroms, async (ifrom: string, ix: number) => {
       // cancel if requested
       if (longRunningOpCancelID === id)
         throw new Error(`File ${op} canceled by request`);
-
-      // if the "to" already exists, we have to disambiguate it
-      const parsed = path.parse(itos[ix]);
-      for (let iy = 1; await fs.pathExists(itos[ix]); iy++)
-        itos[ix] = path.join(parsed.dir, `${parsed.name} (${iy})${parsed.ext}`);
 
       // execute copy or move
       op === 'copy'
@@ -104,27 +122,23 @@ const copyAndMover = async (
           longRunningOp
         );
       }
-    },
-    async (err) => {
-      // whatever happened, we're done
-      longRunningOp.running = false;
-      theWindow?.webContents.send(
-        Channels.longRunningOpProgress,
-        longRunningOp
-      );
+    });
+  } catch (error) {
+    theWindow?.webContents.send(Channels.error, error.message);
+    return;
+  } finally {
+    // always complete the long-running op
+    longRunningOp.running = false;
+    theWindow?.webContents.send(Channels.longRunningOpProgress, longRunningOp);
+  }
 
-      // send an error meesage if we failed
-      if (err) theWindow?.webContents.send(Channels.error, err.message);
-      else {
-        // after a move, make sure that the "from" directory is gone
-        for (const cleanup of cleanups) await fs.remove(cleanup);
-        // send completed message
-        const channel =
-          op === 'copy' ? Channels.fsCopyCompleted : Channels.fsMoveCompleted;
-        theWindow?.webContents.send(channel, id, froms, tos);
-      }
-    }
-  );
+  // after a move, make sure that the "from" directory is gone
+  for (const cleanup of cleanups) await fs.remove(cleanup);
+
+  // send completed message
+  const channel =
+    op === 'copy' ? Channels.fsCopyCompleted : Channels.fsMoveCompleted;
+  theWindow?.webContents.send(channel, id, froms, tos);
 };
 
 ipcMain.on(
